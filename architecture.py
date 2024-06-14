b5f336632048cdccea666c4d13c361681d9bd301
Shajid-M <shajid24mughal@gmail.com>
2024-06-15 00:31:39 +0530

    typos

--- architecture.py
+++ architecture.py
@@ -20,7 +20,7 @@ class PositionalEncodings(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
-        self.droupout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
 
         pe = torch.zeros(seq_len, d_model)
         position = torch.arange(0, self.seq_len).unsqueeze(1)
@@ -35,7 +35,7 @@ class PositionalEncodings(nn.Module):
 
     def forward(self, x):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
-        return self.droupout(x)
+        return self.dropout(x)
 
 
 class LayerNormalization(nn.Module):
@@ -149,12 +149,12 @@ class Encoder(nn.Module):
     
 class DecoderBlock(nn.Module):
 
-    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, droupout: float):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
         super().__init__()
         self.self_attention_block = self_attention_block
         self.cross_attention_block = cross_attention_block
         self.feed_forward_block = feed_forward_block
-        self.residual_connections = nn.ModuleList([ResidualConnection(droupout) for _ in range(3)])
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
 
     def forward(self, x, encoder_output, src_mask, tar_mask):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, tar_mask))
